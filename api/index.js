7849d1faaa096572a043b6c012fb946c8ad670d0
Developer <developer@example.com>
2026-06-16 13:59:48 +0300

    chore: trigger redeployment to reset container

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ require('dotenv').config({ path: require('path').join(__dirname, '../backend/.en
 const app = require('../backend/app');
 
 module.exports = (req, res) => {
+  // Trigger rebuild for deployment update
   if (req.headers['x-matched-path']) {
     const reqUrl = new URL(req.url, `http://${req.headers.host || 'localhost'}`);
     req.url = req.headers['x-matched-path'] + reqUrl.search;
